0a57fa20d23cd636a4bc2739544acc0023a633fa
tmag187 <tmagdziarz@gmail.com>
2019-08-06 01:25:26 -0700

    inital commit for wyr project

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import  Signin  from './Signin';
+import '../App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+      </header>
+      <Signin />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Signin.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+ class Signin extends Component {
+    render() {
+        return (
+            <h3 className='App-header'>
+                Would you rather...
+            </h3>
+        )
+    }
+}
+
+export default Signin;
